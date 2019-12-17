4fae7c28b604aae5e63244954e7f562a2ca81936
Michael Villarreal <mvillarreal0605@gmail.com>
2019-12-16 21:36:28 -0600

    wrote some tests

--- test/controllers/inquiries_controller_test.rb
+++ test/controllers/inquiries_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class InquiriesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get inquiries_create_url
-    assert_response :success
+  test "should create inquiry" do
+    assert_difference('Inquiry.count') do
+      post inquiries_url, params: { inquiry: { name: 'John Doe', email: 'johndoe@gmail.com' } }
+    end
+    assert_redirected_to :root
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success, "Root not set"
   end
 
 end
--- test/models/inquiry_test.rb
+++ test/models/inquiry_test.rb
@@ -4,4 +4,11 @@ class InquiryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save without name and email" do
+  	inquiry = Inquiry.new
+  	assert_not inquiry.save, "Saved a blank inquiry"
+  	assert_not Inquiry.new(name: "name",message: "message", phone_number: "8168764743", duedate: Time.now).save, "Saved without email"
+  	assert_not Inquiry.new(email: "name@example.com", message: "message", phone_number: "8168764743", duedate: Time.now).save, "Saved without name"
+  end
 end
